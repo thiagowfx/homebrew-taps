65ca31901ecbad4e8a67f0a1b31597f19b6b6f5d
Thiago Perrotta <thiago@perrotta.dev>
2025-11-30 00:20:50 -0300

    add checksum to python-is-python3

--- Formula/python-is-python3.rb
+++ Formula/python-is-python3.rb
@@ -3,6 +3,7 @@ class PythonIsPython3 < Formula
   homepage "https://github.com/thiagowfx/homebrew-taps"
   url "file:///dev/null"
   version "1.0"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   depends_on "python"
 
