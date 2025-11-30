ecd26634264e857514c4a89c327e0e771fb38d78
Thiago Perrotta <thiago@perrotta.dev>
2025-11-30 00:14:59 -0300

    new formula: python-is-python3

---
+++ Formula/python-is-python3.rb
@@ -0,0 +1,11 @@
+class PythonIsPython3 < Formula
+  desc "Make python point to python3"
+  version "1.0"
+
+  depends_on "python"
+
+  def install
+    bin.install_symlink Formula["python"].opt_bin/"python3" => "python"
+    bin.install_symlink Formula["python"].opt_bin/"pip3" => "pip"
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# thiagowfx/homebrew-taps
+
+This repository hosts custom Homebrew formulae for the `thiagowfx/taps` tap. To add the tap:
+
+```bash
+brew tap thiagowfx/taps
+```
+
+Formulas should live under `Formula/` following the standard Homebrew structure.
